f2e462b9cfd7ba48206d9c2caea7e7a977c749bc
gcmaziero <gcmaziero@rd.com.br>
2021-11-22 18:52:28 -0300

    Initial Commit

---
+++ marvel-app/marvel_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  marvel_appApp.swift
+//  marvel-app
+//
+//  Created by Gabriel Chaves Maziero on 22/11/21.
+//
+
+import SwiftUI
+
+@main
+struct marvel_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
